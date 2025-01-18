e131c70a68650c7111e45067014a86af3a3fc642
bucurdavid <dbucur22@gmail.com>
2025-01-18 16:16:05 +0200

    feat: types

---
+++ src/core/types.ts
@@ -0,0 +1,88 @@
+export interface BaseManifest {
+  data_stream: {
+    category: string;
+    name: string;
+    creator: string;
+    created_on: string;
+    last_modified_on: string;
+    marshalManifest: {
+      totalItems: number;
+      nestedStream: boolean;
+    };
+  };
+  data: unknown[];
+}
+
+export interface MusicPlaylistConfig {
+  name: string;
+  creator: string;
+  filesMetadata?: Record<string, FileMetadata>;
+  defaultMetadata?: {
+    artist: string;
+    album: string;
+  };
+}
+
+export interface FileMetadata {
+  artist: string;
+  album: string;
+}
+
+export interface MusicPlaylistManifest extends BaseManifest {
+  data: Array<{
+    idx: number;
+    date: string;
+    category: string;
+    artist: string;
+    album: string;
+    cover_art_url: string;
+    title: string;
+  }>;
+}
+
+export type UploadedFile = {
+  hash: string;
+  fileName: string;
+  mimeType: string;
+  folderHash: string;
+  category: string;
+};
+
+export enum ManifestType {
+  MusicPlaylist = 'musicplaylist'
+}
+
+export interface IManifestBuilder {
+  buildManifest(
+    type: ManifestType,
+    files: UploadedFile[],
+    musicPlaylistConfig: MusicPlaylistConfig
+  ): Promise<BaseManifest>;
+}
+
+export interface Logger {
+  closeByNewLine?: boolean;
+  useIcons?: boolean;
+  verbose?: boolean;
+  log: (...strings: any[]) => void;
+  warn: (...strings: any[]) => void;
+  error: (...strings: any[]) => void;
+  info: (...strings: any[]) => void;
+  success: (...strings: any[]) => void;
+  debug: (...strings: any[]) => void;
+}
+
+export interface ICreditManager {
+  handlePayment(numberOfFiles: number): Promise<string>;
+}
+
+export interface IFileManagerParams {
+  files: File | File[];
+  category: 'files' | 'staticdata' | ManifestType;
+  paymentHash: string;
+  address: string;
+}
+
+export interface IFileManager {
+  upload(params: IFileManagerParams): Promise<UploadedFile[]>;
+}
